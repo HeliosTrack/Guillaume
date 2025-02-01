5cbff65058c3b21f46d135fcf61021ea74a093f0
arch <enrsalut@gmail.com>
2025-02-01 23:38:09 +0100

    I can communicate !!!

--- server.py
+++ server.py
@@ -1,14 +1,12 @@
-import socket
+import meshtastic
+import meshtastic.serial_interface
 
-def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 4403))  # Use the correct port
-    server_socket.listen(1)
-    print("Server started and listening on port 4403")
-    while True:
-        client_socket, addr = server_socket.accept()
-        print(f"Connection from {addr}")
-        client_socket.sendall(b"Hello, client!")
-        client_socket.close()
 
-start_server()
+def on_receive(packet, interface):
+    print("Packet reçu :", packet)
+
+interface = meshtastic.serial_interface.SerialInterface()
+interface.onReceive = on_receive
+
+input("Appuyez sur Entrée pour quitter...\n")  # Garde le script en écoute
+interface.close()
--- test_e.py
+++ test_e.py
@@ -1,22 +1,27 @@
 import meshtastic
 import meshtastic.serial_interface
-import meshtastic.mesh_interface
-import meshtastic.version
-import meshtastic.remote_hardware
+from meshtastic import BROADCAST_NUM
+import time
 
-# Print the version of the meshtastic distribution
-print(f"Hi ! This software run on  : {meshtastic.version.version('meshtastic')} meshtastic version")
+def main():
+    interface = meshtastic.serial_interface.SerialInterface()
+    
+    # On attend que l'interface soit connectée et que le nodedb soit disponible
+    while not interface.nodes:
+        print("En attente des nœuds...")
+        time.sleep(1)
 
-# Create an instance of MeshInterface
-mesh_interface_instance = meshtastic.mesh_interface.meshtastic.serial_interface.SerialInterface()
+    # Supposons que node_id soit la destination souhaitée (exemple : "!435a7150")
+    node_id = "!435a7150"
+    print(f"Demande de télémétrie pour le nœud {node_id}...")
+    
+    
+    print(interface.sendTelemetry(destinationId=node_id, wantResponse=True, telemetryType="environment_metrics"))
 
-print(mesh_interface_instance.showInfo())
+    print("En attente de la réponse de télémétrie...")
+    time.sleep(10)
+    
+    interface.close()
 
-# Create an instance of RemoteHardwareClient
-remote_hardware_instance = RemoteHardwareClient()
-
-node_info = mesh_interface_instance.getMyNodeInfo()
-nodeid = node_info["user"]["id"]  # Extract the actual node ID
-mask = 0x76  # Mask for IO16 and IO17
-
-print(remote_hardware_instance.readGPIOs(nodeid=nodeid, mask=mask))
+if __name__ == '__main__':
+    main()
